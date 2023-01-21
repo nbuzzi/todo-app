0e3f8e1f2eb11f97851a157a539b96846f9ea0af
Nicolas Buzzi <nico.cbuzzi@gmail.com>
2023-01-21 14:29:23 -0300

    Updating logic for timer indicator

--- frontend/src/app/timer/timer.component.ts
+++ frontend/src/app/timer/timer.component.ts
@@ -18,19 +18,20 @@ export class TimerComponent {
 
   @Output() onCompleted = new EventEmitter();
   @Output() onChange = new EventEmitter<number>();
-
   @Input() currentTime = 0;
+
   @Input() started = false;
   @Input() paused = false;
   @Input() stopped = true;
-
   public limit: number;
+  public pieTime: number;
 
   constructor() {
     const now = new Date();
     const countDown = moment(now).add(30, "minutes");
     // const countDown = moment(now).add(1, "minute");
-    this.limit = moment(countDown).diff(now, "seconds") + 1;
+    this.limit = moment(countDown).diff(now, "seconds");
+    this.pieTime = this.limit;
   }
 
   ngOnChanges() {
@@ -42,36 +43,31 @@ export class TimerComponent {
   public drawLoader(): void {
     const π = Math.PI;
     const that = this;
-
     let loader = this.timer.nativeElement.querySelector("#loader");
     let border = this.timer.nativeElement.querySelector("#border");
     let svg = this.timer.nativeElement.querySelector("#svg");
     let isCompleted = false;
-
     (function draw() {
       if (isCompleted) {
         that.onCompleted.emit();
         return;
       }
-
+      that.pieTime--;
       that.currentTime++;
-      that.currentTime %= that.limit;
-      let r = (that.currentTime * π) / (that.limit / 2),
+      that.pieTime %= that.limit;
+      let r = (that.pieTime * π) / (that.limit / 2),
         x = Math.sin(r) * 125,
         y = Math.cos(r) * -125,
-        mid = that.currentTime > that.limit / 2 ? 1 : 0,
+        mid = that.pieTime > that.limit / 2 ? 1 : 0,
         anim = "M 0 0 v -125 A 125 125 1 " + mid + " 1 " + x + " " + y + " z";
-
       loader.setAttribute("d", anim);
       border.setAttribute("d", anim);
       svg.setAttribute("viewBox", "0 0 250 250");
-
       that.onChange.emit(that.currentTime);
       if (!that.paused && !that.stopped) {
         if (that.currentTime === that.limit - 1) {
           isCompleted = true;
         }
-
         setTimeout(draw, 1000);
       }
     })();
